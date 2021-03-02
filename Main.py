49d456f5a3e457203abcba0308b6f83aab193327
Tanay Padar <72204355+JuniorProgrammer30104@users.noreply.github.com>
2021-03-02 19:06:57 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,15 @@
+#Audio Book by JP 
+#Youtube Channel -: https://tinyurl.com/1smu75d8
+#Youtube Name -: Junior Programmer 
+
+#Import Modules 
 import pyttsx3
 import PyPDF2
 
-book = open('python_tutorial.pdf','rb')
+#Open File to convert
+book = open('python_tutorial.pdf','rb') #PDF file location 
+
+#Main Loop
 pdfReader=PyPDF2.PdfFileReader(book)
 pages =  pdfReader.numPages
 print (pages)
